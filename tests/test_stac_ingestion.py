03bc8e4a4d7a779415543b3318e27c64e4f77f2f
Emile Tenezakis <e.tenezakis@gmail.com>
2023-08-04 18:30:29 +0300

    small modifs to tests (#12)

--- tests/test_stac_ingestion.py
+++ tests/test_stac_ingestion.py
@@ -4,18 +4,12 @@ from pystac import STACValidationError
 
 # Test validating the collection
 def test_validate_collection(test_collection):
-    try:
-        pystac.validation.validate_dict(test_collection)
-    except STACValidationError as e:
-        pytest.fail(f"Validation failed for test_collection {e}")
-
+    pystac.validation.validate_dict(test_collection)
+    
 
 # Test validating the item
 def test_validate_item(test_item):
-    try:
-        pystac.validation.validate_dict(test_item)
-    except STACValidationError as e:
-        pytest.fail(f"Validation failed for test_item {e}")
+    pystac.validation.validate_dict(test_item)
 
 
 # Test inserting collection
@@ -25,27 +19,27 @@ def test_insert_collection(
     response = stac_ingestion_instance.insert_collection(
         authentication_token, test_collection
     )
-    assert response.status_code in [200, 201], f"Failed to insert the test_collection {response.text}"
+    assert response.status_code in [200, 201], f"Failed to insert the test_collection :\n{response.text}"
 
 
 # Test inserting item
 def test_insert_item(stac_ingestion_instance, authentication_token, test_item):
     response = stac_ingestion_instance.insert_item(authentication_token, test_item)
-    assert response.status_code in [200, 201], f"Failed to insert the test_item {response.text}"
+    assert response.status_code in [200, 201], f"Failed to insert the test_item :\n{response.text}"
 
 
 # Test querying collection and verifying inserted collection
 def test_query_collection(stac_ingestion_instance, test_collection):
     response = stac_ingestion_instance.query_collection(test_collection["id"])
-    assert response.status_code in [200, 201], f"Failed to query the test_collection {response.text}"
+    assert response.status_code in [200, 201], f"Failed to query the test_collection :\n{response.text}"
 
 
 # Test querying items and verifying inserted items
 def test_query_items(stac_ingestion_instance, test_collection, test_item):
     response = stac_ingestion_instance.query_items(test_collection["id"])
-    assert response.status_code in [200, 201], f"Failed to query the items {response.text}"
+    assert response.status_code in [200, 201], f"Failed to query the items :\n{response.text}"
     item = response.json()["features"][0]
-    assert item["id"] == test_item["id"], f"Inserted item - {test_item} \n not found in the queried items {items}"
+    assert item["id"] == test_item["id"], f"Inserted item - {test_item} \n not found in the queried items {item}"
 
 
 # Run the tests
